8f6dcf56c86f857f0dcf29e266b433c9216bd29b
EndrexAkoto <martinakoto3@gmail.com>
2024-04-05 15:04:52 +0300

    update to database

--- backend/dist/server.dev.js
+++ backend/dist/server.dev.js
@@ -2,7 +2,7 @@
 
 var _express = _interopRequireDefault(require("express"));
 
-var _dotenv = _interopRequireDefault(require("dotenv"));
+var _dotenv = _interopRequireDefault(require("./utils/dotenv.js"));
 
 var _mongo = _interopRequireDefault(require("./utils/mongo.js"));
 
@@ -20,10 +20,11 @@ var _helmet = _interopRequireDefault(require("helmet"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-_dotenv["default"].config(); // Load environment variables from .env file
+// import bodyParser from 'body-parser';
+_dotenv["default"];
+var app = (0, _express["default"])(); // app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-
-var app = (0, _express["default"])();
 app.use(_express["default"].json());
 app.use((0, _helmet["default"])());
 var port = process.env.SERVER_PORT;
@@ -38,40 +39,68 @@ if (!app || !port || !type) {
 (0, _mongo["default"])();
 
 _redis["default"].connect(); // Routes for server
+// Default route for the server to check if the server is running
 
 
-app.use(_welcome["default"]);
-app.use(_authRoutes["default"]);
-app.use(_Route["default"]); // Define a route to handle form submissions
+app.use(_welcome["default"]); // Auth routes
 
-app.post('/submit-form', function _callee(req, res) {
-  var formData;
+app.use(_authRoutes["default"]); // Handle form submission
+
+app.post('/api/submit-form', function _callee(req, res) {
+  var newUser;
   return regeneratorRuntime.async(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
-          try {
-            formData = req.body; // Process and save the form data to MongoDB
-            // Example: const savedData = await saveFormDataToDatabase(formData);
+          _context.prev = 0;
+          // Create a new user instance with data from the form
+          newUser = new User({
+            name: req.body.name,
+            profession: req.body.profession,
+            specialization: req.body.specialization,
+            location: req.body.location,
+            address: req.body.address,
+            email: req.body.email,
+            phone: req.body.phone,
+            about: req.body.about,
+            skills: req.body.skills.split(',').map(function (skill) {
+              return skill.trim();
+            }),
+            // Assuming skills are comma-separated
+            education: req.body.education.split(',').map(function (edu) {
+              return edu.trim();
+            }),
+            experience: req.body.experience.split(',').map(function (exp) {
+              return exp.trim();
+            }),
+            courses: req.body.courses.split(',').map(function (course) {
+              return course.trim();
+            })
+          }); // Save the user data to the database
 
-            res.status(200).json({
-              message: 'Form submitted successfully!',
-              data: formData
-            });
-          } catch (error) {
-            console.error('Error:', error);
-            res.status(500).json({
-              error: 'Internal server error'
-            });
-          }
+          _context.next = 4;
+          return regeneratorRuntime.awrap(newUser.save());
 
-        case 1:
+        case 4:
+          res.status(201).send('Form submitted successfully');
+          _context.next = 11;
+          break;
+
+        case 7:
+          _context.prev = 7;
+          _context.t0 = _context["catch"](0);
+          console.error(_context.t0);
+          res.status(500).send('Internal server error');
+
+        case 11:
         case "end":
           return _context.stop();
       }
     }
-  });
-});
+  }, null, null, [[0, 7]]);
+}); // 404 route for the server to handle invalid routes
+
+app.use(_Route["default"]);
 app.listen(port, function () {
   _logger["default"].info("".concat(type, " Server is running on port ").concat(port));
 });
--- backend/utils/dotenv.js
+++ backend/utils/dotenv.js
@@ -1,9 +1,17 @@
+/**
+ * This is a module to load the environment variables
+ * @parent utils
+ * @module dotenv
+ * @requires dotenv
+ * @requires utils/enviroment for getEnvironmentPath function
+ *
+ * @example exports envs from './utils/dotenv.js';
+ * 
+ * @return {Object} envs - The environment variables
+ */
 import dotenv from 'dotenv';
 import getEnvironmentPath from './enviroment.js';
 
-const configureEnvironment = () => {
-  dotenv.config({ path: getEnvironmentPath() });
-  return process.env;
-};
+const envs = dotenv.config({ path: getEnvironmentPath() });
 
-export default configureEnvironment;
+export default envs;
